1d07e6789d36c00823eeffb9a0e111aed5129adc
Gerald <gera2ld@live.com>
2022-01-01 23:14:13 +0800

    feat: implement dark/light theme

--- rollup.conf.js
+++ rollup.conf.js
@@ -1,12 +1,16 @@
-const fs = require('fs');
-const { getRollupPlugins } = require('@gera2ld/plaid');
+const path = require('path');
+const { getRollupPlugins, defaultOptions } = require('@gera2ld/plaid');
+const userscript = require('rollup-plugin-userscript');
 const pkg = require('./package.json');
 
 const DIST = 'dist';
 const FILENAME = 'json-formatter';
-const BANNER = fs.readFileSync('src/meta.js', 'utf8')
-.replace('process.env.VERSION', pkg.version);
 
+const bundleOptions = {
+  extend: true,
+  esModule: false,
+};
+defaultOptions.tailwindcss.darkMode = 'class';
 const postcssOptions = {
   ...require('@gera2ld/plaid/config/postcssrc'),
   inject: false,
@@ -16,15 +20,33 @@ const rollupConfig = [
   {
     input: {
       input: 'src/index.js',
-      plugins: getRollupPlugins({
-        browser: true,
-        postcss: postcssOptions,
-      }),
+      plugins: [
+        {
+          async transform(code, id) {
+            // process CSS from importHttp
+            if (/^\0.*?\.css$/.test(id)) {
+              const { css } = await require('postcss')([require('cssnano')]).process(code, { from: 'src/style.css' });
+              return `export default ${JSON.stringify(css)}`;
+            }
+          },
+        },
+        ...getRollupPlugins({
+          browser: true,
+          postcss: postcssOptions,
+          importHttp: true,
+        }),
+        userscript(
+          path.resolve('src/meta.js'),
+          meta => meta
+            .replace('process.env.VERSION', pkg.version)
+            .replace('process.env.AUTHOR', pkg.author),
+        ),
+      ],
     },
     output: {
       format: 'iife',
       file: `${DIST}/${FILENAME}.user.js`,
-      name: 'iife',
+      ...bundleOptions,
     },
   },
 ];
@@ -32,10 +54,9 @@ const rollupConfig = [
 rollupConfig.forEach((item) => {
   item.output = {
     indent: false,
+    // If set to false, circular dependencies and live bindings for external imports won't work
+    externalLiveBindings: false,
     ...item.output,
-    ...BANNER && {
-      banner: BANNER,
-    },
   };
 });
 
